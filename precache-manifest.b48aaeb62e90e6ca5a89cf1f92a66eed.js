75d7c4009f8e7ebd4f49f198da6a6945f309473f
EdwardBrodskiy <brodskiedward@gmail.com>
2020-09-15 12:07:52 +0100

    Updates

---
+++ precache-manifest.b48aaeb62e90e6ca5a89cf1f92a66eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893a8b88bab9b8ccabcc5b1870de63b6",
+    "url": "/gh-portfolio/index.html"
+  },
+  {
+    "revision": "ad3916102db904bba8d0",
+    "url": "/gh-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e87774d1e6a46669febb",
+    "url": "/gh-portfolio/static/js/2.68b79c96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gh-portfolio/static/js/2.68b79c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3916102db904bba8d0",
+    "url": "/gh-portfolio/static/js/main.a6040a4e.chunk.js"
+  },
+  {
+    "revision": "dfcaa4a95351ead5b56a",
+    "url": "/gh-portfolio/static/js/runtime-main.c61bbf22.js"
+  }
+]);
